b8a490b8a81f8d771256587ce48bab85deda684c
Namanand Maske <124178623+RajMaske@users.noreply.github.com>
2024-06-06 12:00:41 +0530

    Initial commit

---
+++ ChatAppMVC/Controllers/ChatController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAppMVC.Controllers
+{
+    [Route("Chat")]
+    public class ChatController : Controller
+    {
+        [Route("HomePage")]
+        public IActionResult HomePage()
+        {
+            ViewData["Title"] = "Chat";
+            return View();
+        }
+    }
+}
---
+++ ChatAppMVC/Models/ChatViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatAppMVC.Models
+{
+    public class ChatViewModel
+    {
+        [Required]
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+}
